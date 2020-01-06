2974f5ffb7e643b7c137921828c549c20131610e
OnoprienkoArtem <o.artem.e@gmail.com>
2020-01-06 23:40:27 +0200

    add goalitem and goalinput components

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
 
 const GoalInput = props => {
-    return ...
+    return (
+        <View style={styles.listInput}>
+            <Text>{props.title}</Text>
+        </View>
+    )
 }
 
 export default GoalInput;
+
+const styles = StyleSheet.create({    
+    listInput: {
+        
+    }
+  });
